c6e01d87ea34fc307b5701130399ceeaae7e6daa
Mobile User <mobile@ipad-von-manuel.local>
2026-02-10 18:45:14 +0100

    save local changes at 2026-02-10 18:45:14

---
+++ card-left-or-right/main.lua
@@ -0,0 +1,18 @@
+-- main.lua
+local Input				= require("nodes.inpout")
+local Movement 		= require("nodes.movement")
+local Render			= require("nodes.render")
+
+function love.load()
+	inputNode			= Input:new()
+	movementNode	= Movement:new()
+end
+
+function love.update(dt)
+	inputNode:update()
+	movementNode:update(dt, inputNode)
+end
+
+function love.draw()
+	Render:draw/movementNode)
+end
