504a2e7e255beccb1821505fb32f33834a7cb96c
Mobile User <mobile@ipad-von-manuel.local>
2026-02-11 01:41:41 +0100

    save local changes at 2026-02-11 01:41:41

--- card-left-or-right/main.lua
+++ card-left-or-right/main.lua
@@ -1,7 +1,12 @@
 local Input    = require("nodes.input")
 local Movement = require("nodes.movement")
 
-
+function dumpTable(t, name)
+    print("TABLE:", name or "")
+    for k, v in pairs(t) do
+        print("  ", k, "=", v, "(type:", type(v) .. ")")
+    end
+end
 
 function love.load()
     love.window.setMode(800, 600)
@@ -11,13 +16,12 @@ function love.load()
 end
 
 function love.update(dt)
-    inputNode:update()
-    movementNode:update(dt, inputNode)
+	inputNode:update()
+	movementNode:update(dt, inputNode)
+	dumpTable(movementNode, "movementNode")
 end
 
 function love.draw()
     love.graphics.setColor(1, 1, 1)
     love.graphics.circle("fill", movementNode.x, movementNode.y, 20)
-		print(movementNode)
-		print(type(movementNode.update))
 end
